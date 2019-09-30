5ce4f5d70a212f81f367292bdcb648a3ece0081b
DATBOI <alexsimons9999@gmail.com>
2019-09-30 18:14:51 -0500

    These are silly things

--- src/main/kotlin/Monoids.kt
+++ src/main/kotlin/Monoids.kt
@@ -5,12 +5,75 @@ fun main() {
     println(BooleanOr(BooleanOr.zero, true))
     println(BooleanAnd(BooleanAnd.zero, false))
     println(BooleanAnd(BooleanAnd.zero, true))
+    val endoFundie: EndoFunction<Int> = EndoFunction()
+    val adder: (Int) -> Int = { IntAddition(it, it) }
+    println(endoFundie(adder, adder)(1))
+
+    val stringo = listOf("Finna", "bust", "a", "nut")
+    println(stringo.reduceRight(StringConcat.operation))
+    println(stringo.reduceLeft { it }(StringConcat.operation))
+
+    val sentenceBoi = " lorem ipsum dolor sit amet, "
+    println(WordCount(WordCount.zero, sentenceBoi.toStub()))
+    println(WordCount(WordCount.zero, sentenceBoi.trim().toStub()))
+    println(WordCount(WordCount.zero, "famravioli".toStub()))
+    println(WordCount("famravioli".toStub(), WordCount.zero))
+
+}
+
+// Yo dawg, you like Indian food?
+fun <T, R> List<T>.reduceLeft(mapper: (T) -> R): ((R, R) -> R) -> R {
+    return { reducer -> this.asReversed().map(mapper).reduceRight(reducer) }
 }
 
 interface Monoid<T> {
     val zero: T
-
     operator fun invoke(left: T, right: T): T
+
+    val operation: (T, T) -> T
+        get() = { l, r -> invoke(l, r) }
+}
+
+interface WC
+data class Stub(val characters: String) : WC
+data class Part(
+    val leftStub: String,
+    val rightStub: String,
+    val wordCount: Int
+) : WC
+
+fun String.toStub(): Stub = Stub(this)
+
+object WordCount : Monoid<WC> {
+    override val zero: WC
+        get() = Stub("")
+
+    override fun invoke(left: WC, right: WC): WC {
+        val leftPart = if (left is Stub) toPart(left) else left as Part
+        val rightPart = if (right is Stub) toPart(right) else right as Part
+        val splitWord = leftPart.rightStub + rightPart.leftStub
+        val wordCount = leftPart.wordCount + rightPart.wordCount + if (splitWord.isNotBlank()) 1 else 0
+        return Part(leftPart.leftStub, rightPart.rightStub, wordCount)
+    }
+
+    private fun toPart(stub: Stub): Part {
+        val words = stub.characters.split(" ")
+        return Part(
+            words.getOrElse(0) { "" },
+            if(words.size == 1) "" else words.getOrElse(words.size - 1) { "" },
+            if (words.size < 3) 0 else words.size - 2
+        )
+    }
+
+}
+
+object StringConcat : Monoid<String> {
+
+    override fun invoke(left: String, right: String): String = "$left$right"
+
+    override val zero: String
+        get() = ""
+
 }
 
 object IntAddition : Monoid<Int> {
@@ -21,7 +84,7 @@ object IntAddition : Monoid<Int> {
         left + right
 }
 
-object IntMultiplication: Monoid<Int> {
+object IntMultiplication : Monoid<Int> {
     override val zero: Int
         get() = 1
 
@@ -30,7 +93,7 @@ object IntMultiplication: Monoid<Int> {
 
 }
 
-object BooleanOr: Monoid<Boolean> {
+object BooleanOr : Monoid<Boolean> {
     override val zero: Boolean
         get() = false
 
@@ -38,7 +101,8 @@ object BooleanOr: Monoid<Boolean> {
         left || right
 
 }
-object BooleanAnd: Monoid<Boolean> {
+
+object BooleanAnd : Monoid<Boolean> {
     override val zero: Boolean
         get() = true
 
@@ -46,12 +110,11 @@ object BooleanAnd: Monoid<Boolean> {
         left && right
 }
 
-//object EndoFunction<T>: Monoid<(T) -> T> {
-//    override val zero: (T) -> T
-//        get() = { it }
-//
-//    override fun invoke(left: (T) -> T, right: (T) -> T): (T) -> T {
-//        return { it }
-//    }
-//
-//}
+class EndoFunction<T> : Monoid<(T) -> T> {
+    override val zero: (T) -> T
+        get() = { it }
+
+    override fun invoke(left: (T) -> T, right: (T) -> T): (T) -> T {
+        return { leftT -> right(leftT) }
+    }
+}
